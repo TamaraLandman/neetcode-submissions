c1d480f4acb28b61aeb4cb0f54e5d1fecfd0a508
TamaraLandman <110405016+TamaraLandman@users.noreply.github.com>
2026-07-02 07:31:45 -0400

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+
+        l, r = 0, len(s)-1
+
+        while l < r:
+            if s[l].isalnum() == False:
+                l += 1
+            elif s[r].isalnum() == False:
+                r -= 1
+            else: 
+                if s[l] != s[r]:
+                    skip_l = s[l+1 : r+1]
+                    skip_r = s[l : r]
+                    return skip_l == skip_l[::-1] or skip_r == skip_r[::-1]
+
+                l +=1 
+                r -= 1
+        return True
+
+        
